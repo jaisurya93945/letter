36ca1f5aec96b174a9506c073301cad912194e34
jaisurya93945 <94210631+jaisurya93945@users.noreply.github.com>
2026-02-03 19:36:20 +0530

    loading.js

--- js/loading.js
+++ js/loading.js
@@ -1,10 +1,10 @@
 console.log("HELP");
 $(function(){
   $("#git_photo").on({ mouseenter: function(){
-    $(this).attr('src','img/one2.png');
+    $(this).attr('src','img/test2.png');
   },
   mouseleave: function(){
-    $(this).attr('src','img/one1.png');
+    $(this).attr('src','img/test1.png');
   }
   });
 
